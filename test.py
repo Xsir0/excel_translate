b014843fffb14e96637cb37546039ff88c73dfad
Xyj <xyj_learner@163.com>
2025-01-13 13:54:03 +0800

    update

--- test.py
+++ test.py
@@ -12,42 +12,56 @@ def split_list(lst, n):
     return [lst[i:i+n] for i in range(0, len(lst), n)]
 
 
-def translate(content,language:str):
+def translate(content, language: str):
     print('翻译中...')
     try:
-        translated_content = str(llm_translate(json.dumps(content),language))
-        translated_content = translated_content.replace('```json','').replace('```','')
-        return json.loads(translated_content)
+        translated_content = str(llm_translate(json.dumps(content), language))
+        # translated_content = translated_content.replace(
+        #     '```json', '').replace('```', '')
+        # result = json.loads(translated_content)
+        # if len(result) != len(content):
+        #     print('length different: ', len(result), len(content))
+        #     return translate(content, language)
+        # return result
+        return translated_content
     except Exception as e:
-        print('异常: ',e,traceback.format_exc())
+        print('异常: ', e, traceback.format_exc())
         time.sleep(2)
-        translate(content)
+        translate(content, language)
 
-def read_translate(source_file,target_file,language):
+
+def read_translate(source_file, target_file, language):
     df = pd.read_excel(source_file)
-    texts_to_translate = df['A'].astype(str).tolist()
+    texts_to_translate = df['中文'].astype(str).tolist()
 
-    splited_list = split_list(texts_to_translate,30)
-    translated_list = []
-    for item in splited_list:
-        try:
-            translated_texts = translate(item,language)
-            translated_list.extend(translated_texts)
-            print('append, list size: ',len(translated_list))
-            time.sleep(5)
-        except Exception as e:
-            print('异常: ',traceback.format_exc())
-            translate(item)
-    df[language] = translated_list
-    df.to_excel(target_file, index=False)
-    print(f"翻译完成，结果已保存到 {target_file}")
-
+    try:
+        # splited_list = split_list(texts_to_translate, 50)
+        translated_list = []
+        # for item in splited_list:
+        for item in texts_to_translate:
+            try:
+                translated_texts = translate(item, language)
+                with open('result1.txt', 'a', encoding='utf-8') as file:
+                    # file.writelines(line + '\n' for line in translated_texts)
+                    file.write(f"{item} <<>> {translated_texts}")
+                # translated_list.extend(translated_texts)
+                translated_list.append(
+                    translated_texts)
+                print('append, list size: ', len(translated_list))
+                time.sleep(4)
+            except Exception as e:
+                print('异常: ', traceback.format_exc())
+                translate(item, language)
+    except Exception as e:
+        print('global error,', traceback.format_exc())
+    finally:
+        df['泰文'] = translated_list
+        df.to_excel(target_file, index=False)
+        print(f"翻译完成，结果已保存到 {target_file}")
     print(translated_list)
-    
 
 
 if __name__ == "__main__":
-    input_file = 'new_file.xlsx'
-    output_file = 'output.xlsx'
-    read_translate(input_file,output_file,"菲律宾语")
-
+    input_file = '2.xlsx'
+    output_file = '4.xlsx'
+    read_translate(input_file, output_file, "泰文")
